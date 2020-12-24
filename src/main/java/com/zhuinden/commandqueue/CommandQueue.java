ae07bbedfa86408257d8ea95458f410c4be8dffb
Zhuinden <zhuinden@gmail.com>
2020-12-24 01:26:22 +0100

    Add missing license header, use Java 7

--- src/main/java/com/zhuinden/commandqueue/CommandQueue.java
+++ src/main/java/com/zhuinden/commandqueue/CommandQueue.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Gabor Varadi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.zhuinden.commandqueue;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -14,7 +29,7 @@ import javax.annotation.Nullable;
 public class CommandQueue<T> {
     private final long threadId = Thread.currentThread().getId();
 
-    private final ConcurrentLinkedQueue<T> queuedEvents = new ConcurrentLinkedQueue<>();
+    private final ConcurrentLinkedQueue<T> queuedEvents = new ConcurrentLinkedQueue<T>();
     private boolean paused;
     private boolean distinctOnly;
     private int limit = -1;
@@ -38,7 +53,7 @@ public class CommandQueue<T> {
         }
 
         public CommandQueue<T> build() {
-            CommandQueue<T> commandQueue = new CommandQueue<>();
+            CommandQueue<T> commandQueue = new CommandQueue<T>();
             commandQueue.distinctOnly = distinctOnly;
             commandQueue.limit = limit;
             return commandQueue;
